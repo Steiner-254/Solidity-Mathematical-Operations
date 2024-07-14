83946e8655dd4e7c5af57c11b406d02a8a3f267a
steiner-254 <steineralvin3@gmail.com>
2024-07-14 10:58:02 +0300

    update

---
+++ Multiplication.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract gfgMultiply {
+
+	int128 firstNo ;
+	int128 secondNo ;
+	
+	function firstNoSet(int128 x) public {
+		firstNo = x;
+	}
+	
+	function secondNoSet(int128 y) public {
+		secondNo = y;
+	}
+	
+	function multiply() view public returns (int128) {
+		int128 answer = firstNo * secondNo ;
+		return answer;
+	}
+
+}
